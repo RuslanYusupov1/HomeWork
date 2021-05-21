669cbd2dd6805a7802d2955bbf90e214d0ed5a58
Ruslan Yusupov <ruslan.yusubow@yandex.ru>
2021-05-21 05:26:48 +0300

    5/21/2021

--- 20210508/SQL/18.sql
+++ 20210508/SQL/18.sql
@@ -1,9 +1,9 @@
 /*
-Задание: 18 (Serge I: 2003-02-03) задание на два очка
-Найдите производителей самых дешевых цветных принтеров. Вывести: maker, price
+	Задание: 18 (Serge I: 2003-02-03) задание на два очка
+	Найдите производителей самых дешевых цветных принтеров. Вывести: maker, price
 */
 select distinct pt.maker, pr.price 
 from printer as pr
 	join product as pt on pr.model = pt.model
 where pr.price = all(select min(prin.price) from printer as prin where prin.color = 'y')
-	and color = 'y'
+	and pr.color = 'y'
