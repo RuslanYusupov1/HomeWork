6298e51380df0a6b476e2c5f20808f271a7dfdec
Ruslan Yusupov <ruslan.yusubow@yandex.ru>
2021-05-21 05:50:40 +0300

    4/21/2021

--- 20210510/SQL/57.sql
+++ 20210510/SQL/57.sql
@@ -1,28 +1,29 @@
 /*
-Задание: 57 (Serge I: 2003-02-14)
-Для классов, имеющих потери в виде потопленных кораблей и не менее 3 кораблей в базе данных,
- вывести имя класса и число потопленных кораблей
+	Задание: 57 (Serge I: 2003-02-14)
+	Для классов, имеющих потери в виде потопленных кораблей и не менее 3 кораблей в базе данных,
+	вывести имя класса и число потопленных кораблей
 */
 select class as c, count(ship)
 from (
-	select class, ship
-	from Outcomes as o
-		join Ships as s on o.ship = s.name and result = 'sunk'
-      union
-	select c.class, o.ship
-	from Outcomes
-		join Classes as c on o.ship = c.class and result = 'sunk'
-) as t1
-where c.class in (
-	select class
-	from (
-		select class, name
-		from Ships
+		select class, ship
+		from Outcomes as o
+			join Ships as s on o.ship = s.name and result = 'sunk'
 		union
-		select ship, ship
-		from Outcomes
-			join Classes on ship = class
-	) as t2
-group by class
-having count( name ) > 2)
+			select c.class, o.ship
+			from Outcomes
+				join Classes as c on o.ship = c.class and result = 'sunk'
+	) as t1
+where c.class in (
+		select class
+		from (
+				select class, name
+				from Ships
+				union
+				select ship, ship
+				from Outcomes
+					join Classes on ship = class
+			) as t2	
+		group by class
+		having count( name ) > 2
+		)
 group by class
