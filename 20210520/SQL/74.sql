2f602d449fe346b9d7598a55a325ccd9fdf18551
Ruslan Yusupov <ruslan.yusubow@yandex.ru>
2021-05-23 20:11:28 +0300

    change 20210520

--- 20210520/SQL/74.sql
+++ 20210520/SQL/74.sql
@@ -2,20 +2,20 @@
 	Вывести классы всех кораблей России (Russia). Если в базе данных нет классов кораблей России, вывести классы для всех имеющихся в БД стран.
 	Вывод: страна, класс
 */
-select c.country, c.class
+Select c.country, c.class
 from classes as c
-where c.country = 'Russia' 
-	and exists (
-		select c.country, c.class
-		from classes as c
-		where c.country = 'Russia'
-		union all
-			select c.country, c.class
-			from 
-			classes as c 
-			where not exists (
-					select c.country, c.class
-					from classes as class
-					where c.country = 'Russia'
-					)
+where (
+		c.country = 'Russia' 
+			and 'Russia' = any(
+				select c.country 
+				from classes as c
 			)
+	)
+	or (
+		c.country!='Russia' 
+			and not('Russia' = any(
+				select c.country 
+				from classes as c
+			)
+		)
+	)
