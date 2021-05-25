dfeeae8c276837a977b0ee4803029507b0a1f14c
Ruslan Yusupov <ruslan.yusubow@yandex.ru>
2021-05-25 08:31:17 +0300

    commit 20210508

---
+++ 20210508/SQL/16 вариант решения 2.sql
@@ -0,0 +1,12 @@
+/*
+	Задание: 16 (Serge I: 2003-02-03) задание на два очка
+	Найдите пары моделей PC, имеющих одинаковые скорость и RAM. 
+	В результате каждая пара указывается только один раз, т.е. (i,j), но не (j,i),
+	Порядок вывода: модель с большим номером, модель с меньшим номером, скорость и RAM.
+*/
+select b.model, a.model, b.speed, a.ram
+from PC as a
+	join PC as b on b.speed = a.speed
+where b.model > a.model 
+	and b.ram = a.ram
+group by b.model, a.model, b.speed, a.ram
