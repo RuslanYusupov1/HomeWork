530b71f5cab2cef5be96a369a91bc47fe77f5d9f
Ruslan Yusupov <ruslan.yusubow@yandex.ru>
2021-05-27 06:33:32 +0300

    20210508

--- 20210508/SQL/24.sql
+++ 20210508/SQL/24.sql
@@ -2,21 +2,19 @@
 	Задание: 24 (Serge I: 2003-02-03) задание на два очка
 	Перечислите номера моделей любых типов, имеющих самую высокую цену по всей имеющейся в базе данных продукции
 */
-select a.model
-from (
-		select p.model, p.price from pc as p
-		union
-		select l.model, l.price from laptop as l
-		union
-		select pr.model, pr.price from printer as pr
-	) as a
-where price = (
-		select max(b.price)
-		from (
-				select p.model, p.price from pc as p
-				union
-				select l.model, l.price from laptop as l
-				union
-				select pr.model, pr.price from printer as pr
-			) as b
-	)
+with t as (
+	select pc.price, pc.model
+	from PC as pc
+		join Product as pr on pr.model = pc.model
+	union
+	select p.price, p.model
+	from Printer as p
+		join Product as pr on pr.model = p.model
+	union
+	select l.price, l.model
+	from Laptop as l
+		join Product as pr on pr.model = l.model
+)
+select t.model
+from t
+where t.price = (select max(t.price) from t)
