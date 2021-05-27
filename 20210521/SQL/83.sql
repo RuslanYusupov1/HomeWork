c1dcf9fae464d207d7ad60d3766cd7f88523fdf1
Ruslan Yusupov <ruslan.yusubow@yandex.ru>
2021-05-27 03:18:07 +0300

    change 20210521

--- 20210521/SQL/83.sql
+++ 20210521/SQL/83.sql
@@ -10,18 +10,18 @@
 	country=USA
 */
 with t1 as (
-		Select 
-			s.name
-			,case when c.numGuns=8 then 1 else 0 end as c1
-			,case when c.bore=15.0 then 1 else 0 end as c2
-			,case when c.displacement=32000 then 1 else 0 end as c3
-			,case when c.type='bb' then 1 else 0 end as c4
-			,case when s.launched=1915 then 1 else 0 end as c5
-			,case when c.class='Kongo' then 1 else 0 end as c6
-			,case when c.country='USA' then 1 else 0 end as c7
-		from ships s
-			join classes c ON c.class=s.class
-	)
+	Select 
+		s.name
+		,case when c.numGuns=8 then 1 else 0 end as c1
+		,case when c.bore=15.0 then 1 else 0 end as c2
+		,case when c.displacement=32000 then 1 else 0 end as c3
+		,case when c.type='bb' then 1 else 0 end as c4
+		,case when s.launched=1915 then 1 else 0 end as c5
+		,case when c.class='Kongo' then 1 else 0 end as c6
+		,case when c.country='USA' then 1 else 0 end as c7
+	from ships s
+		join classes c ON c.class=s.class
+)
 select name 
 from t1
 where (c1+c2+c3+c4+c5+c6+c7)> =4
--- 20210521/SQL/85.sql
+++ 20210521/SQL/85.sql
@@ -6,7 +6,7 @@
 select pr.maker 
 from product as pr
 group by pr.maker
-having count (distinct type) = 1 
+having count (distinct pr.type) = 1 
 	and (
 		min(pr.type) = 'printer' 
 			or (min(pr.type) = 'pc' and count(pr.model) >= 3)
